68328b46c516f1c3904f344d940d574cec75e261
Bararuchi <bararuchi1@hotmail.com>
2022-08-27 20:10:02 +0530

    first commit

---
+++ src/main/java/com/dedsec/RedditCloneDemo/RedditCloneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dedsec.RedditCloneDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneDemoApplication.class, args);
+	}
+
+}
